4d94bbe762853ac8dc408aacbff18e1fdd0db12b
Luc Schlosser <schloss444>
2020-06-10 16:11:17 +0200

    ajout route Login

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="ESPACE SECURISE by Luc ........ Vue.js"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginWorld from '@/components/LoginWorld.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    LoginWorld
+  }
+}
+</script>
