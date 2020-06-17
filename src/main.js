4a2dd03bf476a1cda97cc80961b77574ba349862
Luc Schlosser <schloss444>
2020-06-17 16:31:57 +0200

    commit dde 16h31 ce mercredi

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
