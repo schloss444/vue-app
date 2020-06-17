8a127f2fc455f1aae0ae7d487b5311ffd3f6ef8d
Luc Schlosser <schloss444>
2020-06-17 16:36:11 +0200

    ETC

---
+++ docs/precache-manifest.160b442c4eae18dcaa1b9883c36d139a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657979cc0056eb44a96c",
+    "url": "https://schloss444.github.io/vue-app/css/app.3897d836.css"
+  },
+  {
+    "revision": "6c7f5afb08f71f2a0156",
+    "url": "https://schloss444.github.io/vue-app/css/chunk-vendors.084c0951.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "https://schloss444.github.io/vue-app/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cdc08c11528ab9b4d0e79c284cb0e389",
+    "url": "https://schloss444.github.io/vue-app/img/luc.cdc08c11.jpg"
+  },
+  {
+    "revision": "d6ed75ecb93067f16aabf58575bac289",
+    "url": "https://schloss444.github.io/vue-app/index.html"
+  },
+  {
+    "revision": "f842fdc2c65cad8ad760",
+    "url": "https://schloss444.github.io/vue-app/js/about.9c9ead96.js"
+  },
+  {
+    "revision": "657979cc0056eb44a96c",
+    "url": "https://schloss444.github.io/vue-app/js/app.e2e137c6.js"
+  },
+  {
+    "revision": "6c7f5afb08f71f2a0156",
+    "url": "https://schloss444.github.io/vue-app/js/chunk-vendors.516e43c9.js"
+  },
+  {
+    "revision": "a7bb829f05709f04c4cc8f0bb0289d89",
+    "url": "https://schloss444.github.io/vue-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://schloss444.github.io/vue-app/robots.txt"
+  }
+]);
